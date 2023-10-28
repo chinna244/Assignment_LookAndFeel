fd3f433a2f40e50b1126166afcafb1d26baafb94
Hareesh Prathipati <37666700+chinna244@users.noreply.github.com>
2023-10-28 16:19:56 -0400

    Add files via upload

---
+++ widgets.nrel.gov/afdc/station-locator/assests/embed.js
@@ -0,0 +1,18 @@
+!function(){
+    function t(t){
+        var s=document.createElement("script");
+        s.type="text/javascript",
+        s.async=!1,s.crossOrigin="anonymous",
+        s.src=t,document.body.appendChild(s)
+    }
+    function s(t){
+        var s=document.createElement("link");
+        s.rel="stylesheet",
+        s.href=t,document.body.appendChild(s)
+    }
+    s("https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i"),
+    s("https://widgets.nrel.gov/afdc/station-locator/css/app.5b72b094.css"),
+    t("https://widgets.nrel.gov/afdc/station-locator/js/chunk-vendors.77bb276c.js"),
+    t("https://widgets.nrel.gov/afdc/station-locator/js/app.540d22fa.js")
+}();
+//# sourceMappingURL=embed.js.map
