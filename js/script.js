b151fb2b6f7b2d65701ba75937acb33343e5e1e8
chinna244 <hareeshp@usf.edu>
2023-10-28 20:47:11 -0400

    updated index and search pages

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    var button = document.getElementById("start-button");
+    button.addEventListener("click", function () {
+      // Redirect to the search page
+      window.location.href = "search.html";
+    });
+  });
+  
