a2d3d3f634a3b611bddec281f1543349e8c49879
chinna244 <hareeshp@usf.edu>
2023-11-01 21:29:32 -0400

    Changes in Search Page

--- widgets/afdc/station-locator/assests/embed.js
+++ widgets/afdc/station-locator/assests/embed.js
@@ -11,8 +11,8 @@
         s.href=t,document.body.appendChild(s)
     }
     s("https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i"),
-    s("https://widgets.nrel.gov/afdc/station-locator/css/app.5b72b094.css"),
-    t("https://widgets.nrel.gov/afdc/station-locator/js/chunk-vendors.77bb276c.js"),
-    t("https://widgets.nrel.gov/afdc/station-locator/js/app.540d22fa.js")
+    s("widgets/afdc/station-locator/css/app.5b72b094.css"),
+    t("widgets/afdc/station-locator/js/chunk-vendors.77bb276c.js"),
+    t("widgets/afdc/station-locator/js/app.540d22fa.js")
 }();
 //# sourceMappingURL=embed.js.map
