368abd1608bdc3414966ccf9aabb6e811071af7e
Dharma Rakshak Tadi <dharmarakshaktadi@gmail.com>
2023-11-04 23:28:17 -0400

    changes for bar graph

--- js/script.js
+++ js/script.js
@@ -7,3 +7,154 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+  const apiUrl = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=hkZVQijE8ZvMpchuICzQYDI8gIyPjxVsGzt9h0oI";
+
+// Mapping of state abbreviations to full state names for all U.S. states
+const stateAbbreviationsToNames = {
+  AL: "Alabama",
+  AK: "Alaska",
+  AZ: "Arizona",
+  AR: "Arkansas",
+  CA: "California",
+  CO: "Colorado",
+  CT: "Connecticut",
+  DE: "Delaware",
+  FL: "Florida",
+  GA: "Georgia",
+  HI: "Hawaii",
+  ID: "Idaho",
+  IL: "Illinois",
+  IN: "Indiana",
+  IA: "Iowa",
+  KS: "Kansas",
+  KY: "Kentucky",
+  LA: "Louisiana",
+  ME: "Maine",
+  MD: "Maryland",
+  MA: "Massachusetts",
+  MI: "Michigan",
+  MN: "Minnesota",
+  MS: "Mississippi",
+  MO: "Missouri",
+  MT: "Montana",
+  NE: "Nebraska",
+  NV: "Nevada",
+  NH: "New Hampshire",
+  NJ: "New Jersey",
+  NM: "New Mexico",
+  NY: "New York",
+  NC: "North Carolina",
+  ND: "North Dakota",
+  OH: "Ohio",
+  OK: "Oklahoma",
+  OR: "Oregon",
+  PA: "Pennsylvania",
+  RI: "Rhode Island",
+  SC: "South Carolina",
+  SD: "South Dakota",
+  TN: "Tennessee",
+  TX: "Texas",
+  UT: "Utah",
+  VT: "Vermont",
+  VA: "Virginia",
+  WA: "Washington",
+  WV: "West Virginia",
+  WI: "Wisconsin",
+  WY: "Wyoming",
+};
+
+let chart; // Declare a global variable to store the chart instance
+let originalData; // Store the original data
+ 
+
+
+
+// Fetch data from the API and create the chart
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    // Store the original data
+    originalData = data;
+
+    // Process the data and create a chart
+    createFilteredStateStationsChart(data);
+	createPieChart(data, "all");
+
+	        })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
+
+function createFilteredStateStationsChart(data) {
+  // Extract the relevant data from the API response
+  const stations = data.fuel_stations;
+  const stateCounts = {};
+
+  stations.forEach(station => {
+    const state = station.state;
+    const fullStateName = stateAbbreviationsToNames[state];
+    if (fullStateName) {
+      if (stateCounts[fullStateName]) {
+        stateCounts[fullStateName]++;
+      } else {
+        stateCounts[fullStateName] = 1;
+      }
+    }
+  });
+
+  // Filter out states with null values
+  const filteredStateLabels = Object.keys(stateCounts).filter(
+    state => stateCounts[state] !== null
+  );
+  const filteredStateData = filteredStateLabels.map(state => stateCounts[state]);
+
+  // Check if a chart instance already exists and destroy it
+  if (chart) {
+    chart.destroy();
+  }
+
+  // Create a bar chart
+  const ctx = document.getElementById("stateStationsChart").getContext("2d");
+  chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: filteredStateLabels,
+      datasets: [
+        {
+          label: "Number of Stations",
+          data: filteredStateData,
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+
+  // Add an event listener to the fuel type select input
+  const fuelTypeSelect = document.getElementById("fuelTypeSelect");
+  fuelTypeSelect.addEventListener("change", () => {
+    const selectedFuelType = fuelTypeSelect.value;
+    updateChartByFuelType(selectedFuelType);
+  });
+}
+
+function updateChartByFuelType(selectedFuelType) {
+  if (selectedFuelType === "all") {
+    // Display all data
+    createFilteredStateStationsChart(originalData);
+  } else {
+    // Filter data by the selected fuel type
+    const filteredData = originalData.fuel_stations.filter(station => station.fuel_type_code === selectedFuelType);
+    createFilteredStateStationsChart({ fuel_stations: filteredData });
+  }
+}
+
+
