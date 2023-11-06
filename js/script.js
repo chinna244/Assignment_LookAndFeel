d0356291b794cd245d96206076bdc44170d92730
chinna244 <hareeshp@usf.edu>
2023-11-06 12:30:47 -0500

    Edited filenames for Search.html

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var button = document.getElementById("start-button");
     button.addEventListener("click", function () {
       // Redirect to the search page
-      window.location.href = "search.html";
+      window.location.href = "Search.html";
     });
   });
   
